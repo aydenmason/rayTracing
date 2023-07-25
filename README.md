8243cb49a18969bd02af599a61769090cd4b25d8
aydenmason <aydenjmason@gmail.com>
2023-07-24 17:15:40 -0700

    back to windows

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 #  graphics , rayTracing / rasterizing 
 Using this [book](https://gabrielgambetta.com/computer-graphics-from-scratch/) to learn the fundamentals
 <br /><br />
-NOT a 1:1 Translation of the pseudocode. 
-
 ## [RayLib](https://www.raylib.com)
 golang raylib [bindings](https://pkg.go.dev/github.com/gen2brain/raylib-go/raylib)
--- main.go
+++ main.go
@@ -4,21 +4,70 @@ import rl "github.com/gen2brain/raylib-go/raylib"
 
 var red = []uint32{255,0,0,255}
 
+type sphere struct { 
+	radius float32,
+	color []uint8,
+	center []float32
+}
+
+/*
+O = (0, 0, 0)
+for x = -Cw/2 to Cw/2 {
+    for y = -Ch/2 to Ch/2 {
+        D = CanvasToViewport(x, y)
+        color = TraceRay(O, D, 1, inf)
+        canvas.PutPixel(x, y, color)
+    }
+}
+*/
+var sphere1 = sphere { 
+	radius = 1 
+	center = 
+}
 func main() {
-	rl.InitWindow(800, 800, "raylib [core] example - basic window")
+	rl.InitWindow(1920, 1080, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
+	origin := {0, 0, 0}
+	
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		draw_triangle(0,0,0,50,50,50)
+		for x := -960; x < 960; x++{
+			for y := -540 ; y < 540; y++{
+				D := CanvasToViewPort(x, y)
+				color := TraceRay(origin, D, 1, 9999999)
+			}
+		}
+		//draw_triangle(0,0,0,50,50,50)
 
 		rl.EndDrawing()
 	}
 }
+func CanvasToViewPort(x int, y int){
+	return {x,y,2}
+}
+
+func TraceRay(O []int, D []int, t_min int, t_max int){
+	closest_t = 9999999
+	closest_sphere = nil 
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 //x2-x1,y2-y1
+/*
 func calc_vector(x1 float32, y1 float32,x2 float32, y2 float32)(float32,float32){
 	return  x2-x1,y2-y1
 }
@@ -40,3 +89,4 @@ func draw_line(x float32, y float32, a float32, b float32){
 	}
 
 }
+*/
